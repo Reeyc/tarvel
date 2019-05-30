e583d9da9fef8dfbe18ac4050fd8f8ffa736e31c
Reeyc <helloyec@163.com>
2019-05-30 17:47:13 +0800

    更新header

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import '@/assets/styles/reset.css' //初始化样式
-import '@/assets/styles/border.css' //解决移动端1pxborder样式
-import fastClick from 'fastClick'
+import 'styles/reset.css' //初始化样式
+import 'styles/border.css' //解决移动端1pxborder问题
+import 'styles/iconfont.css' //字体图标
+
+import fastClick from 'fastClick' //解决移动端点击300ms问题
 
 fastClick.attach(document.body)
 Vue.config.productionTip = false
---
+++ src/pages/Home/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe633;</span>
+    </div>
+    <div class="header-search">
+      <span class="iconfont">&#xe615;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  display: flex
+  line-height: 0.86rem
+  background-color: $bgColor
+  color: #fff
+  text-align: center
+.header-left
+  width: 0.64rem
+  float: left
+.header-search
+  flex: 1
+  color: #ccc
+  border-radius: 0.1rem
+  background-color: #fff
+  margin-top: 0.12rem
+  margin-left: 0.2rem
+  height: 0.64rem
+  line-height: 0.64rem
+  text-align: left
+  padding-left: 0.2rem
+.header-right
+  width: 1.24rem
+  float: right
+.header-right>span
+  font-size: 0.24rem
+</style>
+
